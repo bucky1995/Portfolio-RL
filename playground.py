a67317e0db1d96138266642b263475fc49823ab6
bucky1995 <xinbo1995@outlook.com>
2019-12-05 14:50:13 -0500

    add

---
+++ playground.py
@@ -0,0 +1,15 @@
+from data import environment
+
+
+env = environment.Environment()
+
+start_date = "2017/1/4"
+
+end_date = "2017/1/26"
+
+codes_num = 1
+
+res = env.get_repo(start_date,end_date,codes_num)
+
+
+
